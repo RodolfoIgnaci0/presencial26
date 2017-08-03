abd9325214f3b380a4a2cc6d0526d36f4a596e8e
rodolfo <rodolfo.perezg0@gmail.com>
2017-08-03 18:59:35 -0400

    first commit

---
+++ todo_app/app/controllers/todos_controller.rb
@@ -0,0 +1,55 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to todos_path
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update(todo_params)
+    redirect_to todos_path
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  def complete
+    @todo = Todo.find(params[:id])
+    @todo.completed = true
+    @todo.save
+    redirect_to todos_path
+  end
+
+  def list
+    @todos = Todo.all
+    # quedaron al revez en la bd por alguna razon
+    @completed = @todos.select{ |i| i.completed != true }
+    # quedaron al revez en la bd por alguna razon
+    @incompleted = @todos.select{ |i| i.completed != false }
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
+end
